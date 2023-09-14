5366987b782b14073cd9a1d85a37ce62ccf78ad0
Anastasiia Perih <anastasiia.perih@gmail.com>
2023-09-14 18:28:46 +0200

    authorization FE

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import s from './Navigation.module.css'
 
-const Navigation = () => {
+const Navigation = ({ onRouteChange }) => {
    return <nav className={s.nav}>
-      <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+      <p
+         onClick={() => onRouteChange('signin')}
+         className='f3 link dim black underline pa3 pointer'
+      >Sign Out</p>
    </nav>
+
 }
 
 export default Navigation
