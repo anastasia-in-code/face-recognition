9077daa501fc3374aa1a1b56f5aea8708350b780
Anastasiia <anastasiia.perih@gmail.com>
2023-10-05 21:17:36 -0400

    comments

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Rank = (props) => {
-   const {name, rank} = props
+   const { name, rank } = props
    return <div>
       <div className="white f3">
          {`${name}, your current rank is ...`}
